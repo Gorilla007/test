51ac66b48d2950a498e71a5b471666667421ffe6
Fors <doctorwebler@gmail.com>
2017-05-27 13:46:29 +0300

    Test5

--- post.php
+++ post.php
@@ -1,19 +1,16 @@
 <?php
 require_once __DIR__ . '/fb/src/Facebook/autoload.php';
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+
+$params = file_get_contents('http://gorillatv.16mb.com/key.php');
+$params = json_decode($params, true); 
 
 // App ID и App Secret из настроек приложения
-$app_id = "1906904016262094";
-$app_secret = "0dd5fa6b651acac12159514dcafbd75e"; 
+$app_id = $params['app_id'];
+$app_secret = $params['app_secret']; 
 // ID страницы и токен
-$page_id = "100012282824788";
-$secretClient = '890a07907ea193ed88ea0e73c66cf5c5';
-$page_id = "329042160848542"; //page id
-$group_id = "482208485311252";   // group id
-$object_id = "330078190744939";   // object id
-$token = 'EAAbGUbCCk84BAIpVHDTDdF100qocQVXuzZCV2UHltIdJxPZAVDZAuO51feVPGYbXZAmD1PyfUGgFtIG7l58bkCIZAvYXtqmbKFGVHzJHPRZBaJYMoK8D5XqmyoUbwgG0356AeM07v4fZAAkMOst3JRwhji0fjqtoL4ZD';
-
+$page_id = $params['page_id']; //page id
+$group_id = $params['group_id'];   // group id
+$token = $params['token'];
  
 $fb = new Facebook\Facebook(array(
 'app_id'  => $app_id,
@@ -24,7 +21,7 @@ $fb = new Facebook\Facebook(array(
 // описание параметров есть в документации
 $linkData = array(
 'link' => 'https://www.facebook.com/groups/gorillatv/',
-'message' => 'Смотрим!',
+'message' => 'Присоединяйтесь!',
 );
 
 
